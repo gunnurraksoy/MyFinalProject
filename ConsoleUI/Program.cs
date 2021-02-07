426cef71f1af8ef9381646bb82f03426417c1681
gunnurraksoy@gmail.com <gunnurraksoy@gmail.com>
2021-02-07 20:45:56 +0300

    MyFinalProjectAdd project files.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleUI
             ProductManager productManager = new ProductManager(new InMemoryProductDal());
             foreach (var product in productManager.GetAll())
             {
+
                 Console.WriteLine(product.ProductName);
             }
         }
