b14f465d8efa5aef657077a7675928115800b50b
gunnurraksoy@gmail.com <gunnurraksoy@gmail.com>
2021-02-20 17:32:25 +0300

    10.ders sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,18 +9,22 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            //ProductTest();
+            
             //CategoryTest();
-            //ProductTest2();
+            //ProductTest();
 
         }
 
-        private static void ProductTest2()
+        private static void ProductTest()
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var product in productManager.GetProductDetails())
+            var result = productManager.GetProductDetails();
+            if (result.Success==true)
             {
-                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+                }
             }
         }
 
@@ -33,14 +37,8 @@ namespace ConsoleUI
             }
         }
 
-        private static void ProductTest()
-        {
-            ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var product in productManager.GetAllByCategoryId(2))
-            {
-                Console.WriteLine(product.ProductName);
-            }
-        }
+        
+        
     }
 
 }
